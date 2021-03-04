b6d851854c94467d45b1598f5d8e2a8263ffc986
darktend <vladgodunqwer@gmail.com>
2021-03-04 22:42:36 +0200

    i done task typical-array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0
+    }
 
-exports.min = function min (array) {
-  return 0;
+   let minArr=array[0];
+   array.forEach(element => {
+       minArr=element<minArr?element:minArr
+   });
+   return minArr
 }
 
-exports.max = function max (array) {
-  return 0;
+
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0
+    }
+
+   let maxArr=array[0];
+   array.forEach(element => {
+    maxArr=element>maxArr?element:maxArr
+   });
+   return maxArr
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if(!array || array.length===0){
+        return 0
+    }
+    let sum = 0;
+    array.forEach((item) => {
+        sum += item;
+    })
+
+    return sum / array.length;
+
 }
